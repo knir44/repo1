71cc787237ebfd2cd80b34c3ad8fb4b5ed016e1f
knir44 <knir4@gmail.com>
2023-07-17 10:34:02 +0300

    Last one

--- NirKatz.java
+++ NirKatz.java
@@ -1,7 +1,7 @@
 public class NirKatz{
 public static void main(String[] args) {
 for (int i=1;i<=25; i++) {
-System.out.println("My name is Nir Katz "+i);
+System.out.println("I learn DevOps "+i);
 }
 }
 }
